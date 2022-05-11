aff4b21caf3177d962a2478f0c2b78eda14ad1e7
Lars Steinhardt <lars.steinhardt@bluewin.ch>
2022-05-11 21:15:33 +0200

    Commit Model Klassen

---
+++ Bookshelf/src/main/java/ch/bzz/bookshelf/Publisher.java
@@ -0,0 +1,39 @@
+package ch.bzz.bookshelf;
+
+import java.util.List;
+
+/**
+ * the publisher of one or more Books
+ */
+
+
+public class Publisher {
+
+    private String publisherUUID;
+    private String publisher;
+    private List<Book> Booklist;
+
+    public String getPublisherUUID() {
+        return publisherUUID;
+    }
+
+    public void setPublisherUUID(String publisherUUID) {
+        this.publisherUUID = publisherUUID;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public List<Book> getBooklist() {
+        return Booklist;
+    }
+
+    public void setBooklist(List<Book> booklist) {
+        Booklist = booklist;
+    }
+}
